90bac5588d857ed17edfecf3599f892e65da679e
heberpedro <heberv.pedro@gmail.com>
2021-12-28 20:20:19 -0300

    chore: add lint tools

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (_, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.get('/', (_, response) => {
+  return response.json({ message: 'Hello World' })
+})
 
 app.listen(3333, () =>
-  console.log("🔥 Server stared at http://localhost:3333!")
-);
+  console.log('🔥 Server stared at http://localhost:3333!'),
+)
