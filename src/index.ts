74c3f3c2a57449799a14629f41982f01b0f5b23f
heberpedro <heberv.pedro@gmail.com>
2021-12-28 19:41:48 -0300

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+app.listen(3333, () =>
+  console.log("🔥 Server stared at http://localhost:3333!")
+);
